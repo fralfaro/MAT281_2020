0af03c6a91463bed60f7c3172f4791bb922f19a8
Francisco Alfaro Medina <francisco.alfaro.496@gmail.com>
2020-12-05 12:15:14 -0300

    proyecto

---
+++ Project/preprocesamiento.py
@@ -0,0 +1,1 @@
+hacer la mejor predicción de cada imagen. Para ellos es necesario realizar los pasos clásicos de un proyecto de _Machine Learning_, como estadística descriptiva, visualización y preprocesamiento.
